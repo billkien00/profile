e6e1168a371fa51e938fc3ccecd337b4a7b9d014
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-07 16:28:01 +0900

    done

--- src/component/BannerComponent.js
+++ src/component/BannerComponent.js
@@ -1,19 +1,11 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 
 export default function BannerComponent() {
-  const position = {
-    position: "absolute",
-    left: "63%",
-    bottom: "-15%",
-  };
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
   return (
-    <div>
-      <img
-        src="../asset/3560685.jpg"
-        alt="banner"
-        width="100%"
-        style={{ position: "relative" }}
-      ></img>
-    </div>
+    <animated.div style={props}>
+      <img src="../asset/3560685.jpg" alt="banner" width="100%"></img>
+    </animated.div>
   );
 }
--- src/component/ProfileComponent.js
+++ src/component/ProfileComponent.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 
 export default function ProfileComponent() {
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
   return (
-    <div>
+    <animated.div style={props}>
       <img src="../asset/profile.png" alt="profile" width="100%" />
-    </div>
+    </animated.div>
   );
 }
